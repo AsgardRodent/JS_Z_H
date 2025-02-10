d828b17abe87f1a811e49e048634c9767cdd0ef6
Gaurav Hungund <60032249+AsgardRodent@users.noreply.github.com>
2025-02-09 16:22:51 -0800

    Day 2

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,14 @@
+let myName = "Gaurav"
+let myLastName = "Hungund"
+let myCountry = "India"
+let myContinent = "Asia"
+let myContinent_population = "1.429 Billion"
+let isIsland = false
+let language
+
+console.log(`Hello Nice to meet you , I am ${myName} ${myLastName}, I am from ${myCountry} , which is a part of the ${myContinent} continent and has a population of ${myContinent_population}`)
+
+console.log(typeof isIsland)
+console.log(typeof myContinent_population)
+console.log(typeof myCountry)
+console.log(typeof language)
